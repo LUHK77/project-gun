526189bfff9d7116ae229cd0a41b202a335e833e
Maria Eduarda Linck <dudacostalinck@gmail.com>
2026-06-23 17:11:22 -0300

    Melhoria: Menu criado

--- js/main.js
+++ js/main.js
@@ -1,46 +1,86 @@
 // js/main.js
-import { ctx, canvas, drawMap } from './map.js';
+
+import { ctx, drawMap } from './map.js';
 import { player } from './Models/Player.js';
 import { updateEnemies, drawEnemies } from './Models/Enemy.js';
 import { gun } from './Models/Pistol.js';
 import { angulo, mouse } from './Models/Gun.js';
 import { updateBullets, drawBullets } from './Models/Bullet.js';
 import { spawnEnemy } from './spawner.js';
-import { initUI, updateTimer, drawTimer, drawGameOver } from './ui.js';
 
-// Listener de tiro
-window.addEventListener("click", () => gun.atirar(angulo));
+import {
+    initUI,
+    updateTimer,
+    drawTimer,
+    drawGameOver,
+    drawMenu,
+    jogoIniciado
+} from './ui.js';
 
-// Inicia UI e spawn
+// Clique para atirar
+window.addEventListener("click", () => {
+
+    if (!jogoIniciado) return;
+    if (player.hp <= 0) return;
+
+    gun.atirar(angulo);
+});
+
+// Inicializa a UI
 initUI(spawnEnemy);
-spawnEnemy();
 
 let ultimoFrame = 0;
 
 function gameLoop(timestamp) {
+
     const deltaTime = (timestamp - ultimoFrame) / 1000;
     ultimoFrame = timestamp;
 
+    ctx.clearRect(0, 0, 1440, 850);
+
+    // MENU
+    if (!jogoIniciado) {
+
+        drawMenu();
+
+        requestAnimationFrame(gameLoop);
+        return;
+    }
+
+    // JOGO RODANDO
     if (player.hp > 0) {
+
         player.update(deltaTime, mouse);
+
         updateBullets(deltaTime);
+
         updateEnemies(deltaTime);
+
         gun.update(deltaTime);
+
         updateTimer(spawnEnemy);
     }
 
-    ctx.clearRect(0, 0, 1440, 850);
     drawMap(player.x, player.y);
+
     drawEnemies();
+
     drawBullets();
+
     player.draw(mouse);
+
     gun.draw();
     gun.drawBarra();
+
     drawTimer();
 
-    if (player.hp <= 0) drawGameOver();
+    if (player.hp <= 0) {
+        drawGameOver();
+    }
 
     requestAnimationFrame(gameLoop);
 }
 
-document.fonts.ready.then(() => gameLoop(0));
+document.fonts.ready.then(() => {
+    requestAnimationFrame(gameLoop);
+});
